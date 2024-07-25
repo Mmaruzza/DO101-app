b613d082c41858860ca3def460787d95b2bb5053
Mmaruzza <mmaruzza@trans.com.ar>
2024-07-25 10:17:01 -0300

    test openshift

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  response = "This is version 2 of the app" + "\n";
+  res.send(response);
 });
 
 app.listen(port, () => {
