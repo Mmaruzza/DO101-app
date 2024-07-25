01c4e0975d1bec5098061f32d61364156784a1f3
Mmaruzza <mmaruzza@trans.com.ar>
2024-07-25 10:30:34 -0300

    test

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 
 app.get("/", function (req, res) {
   response = "This is version 3 of the app." + "\n";
   res.send(response);
-});
-
-app.get("/test", (req, res) => {
-  response = "This is version 2 of the app" + "\n";
-  res.send(response);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 console.log("Hello World!\n");
